ee89d3724ddeefb052d8256473bc2666404cd039
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-19 16:38:11 +0300

    revised

--- src/what-season.js
+++ src/what-season.js
@@ -1,15 +1,18 @@
-const CustomError = require("../extensions/custom-error");
-module.exports = function getSeason(date) {
-    if (date == undefined) {
+const CustomError = require( "../extensions/custom-error" );
+
+module.exports = function getSeason( date ) {
+
+    if ( !date )
         return 'Unable to determine the time of year!';
-    }
-    if (Object.prototype.toString.call(date) !== '[object Date]') {
+
+    if ( date.toString().indexOf(String(date.getFullYear())) === -1) {
         throw "Error";
     }
-    return Season[date.toString().split(" ")[1]];
+    const season = [ 'winter', 'spring', 'summer', 'autumn' ]
+    let month = date.getMonth();
+    if ( month <= 1 ) return season[ 0 ];
+    if ( month <= 4 ) return season[ 1 ];
+    if ( month <= 7 ) return season[ 2 ];
+    if ( month <= 10 ) return season[ 3 ];
+    return season[ 0 ];
 };
-
-let mounth = {
-    "Dec": "winter", "Jan": "winter", "Feb": "winter", "Mar": "spring", "Apr": "spring", "May": "spring", "Jun": "summer", "Jul": "summer", "Aug": "summer", "Sep": "autumn", "Oct": "autumn", "Nov": "autumn"
-
-}
