8849fb2562897cec9ca72c07a74b7052828cf5e6
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-19 13:08:02 +0300

    feat: 1st attemp

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!== date) {
+        return 'Unable to determine the time of year!';
+    }
+    if (Object.prototype.toString.call(date) !== '[object Date]') {
+        throw "Error";
+    }
+    return Season[date.toString().split(" ")[1]];
 };
+
+let mounth = {
+    "12": "winter", "01": "winter", "02": "winter", "03": "spring", "04": "spring", "05": "spring", "06": "summer", "07": "summer", "08": "summer", "09": "autumn", "10": "autumn", "11": "autumn"
+}
