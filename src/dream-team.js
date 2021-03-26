b4cdf97d4bbae1b8836d3da864e2dc73ef0b827f
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-27 02:22:23 +0800

    init: 1-st attemp

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false
+  }
+
+  let team = []
+  let bezpr = ''
+
+  for(let i = 0; i < members.length; i++) {
+    if(typeof members[i] == "string") {
+      bezpr = members[i].trim()
+      team.push( bezpr[0].toUpperCase())
+    }
+  }
+  return team.sort().join('')
+}
