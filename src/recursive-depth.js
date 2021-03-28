d5f812d3c0d13b96d0166b6709cfc8bcb96b9c46
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-28 20:11:45 +0300

    feat: ...

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,3 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {
-  calcDepth(arr) {
-    let counter = 1;
-    let max = 0;
-    arr.forEach(el => {
-           if (Array.isArray(el)) {
-              max = Math.max(this.calcDepth(el), max);
-            }
-    });
-    return counter + max;
-    };
-};
+
