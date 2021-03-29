bab915c67a9f59361aad9321b27df7140cd2529e
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-30 00:53:34 +0300

    feat: 1-st attemp

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if(!Array.isArray(arr)) {
+    throw new Error();
+  }
+  
+  let newArr = [];
+  var i = 0;
+
+       while (i < arr.length) {
+        if (arr[i] == '--discard-next' && (i != arr.length - 1)) {
+          if ((arr[i + 2] == '--discard-prev') || (arr[i + 2] == '--double-prev')) {
+            i += 3;
+          } else {
+            i += 2; 
+          }
+        } else if (arr[i] == '--double-next' && (i != arr.length - 1)) {
+            newArr.push(arr[i + 1]);
+            newArr.push(arr[i + 1]);
+            i += 2;
+        } else if (arr[i] == '--discard-prev' && (i != 0)) {
+            newArr.pop();
+            i++;
+        } else if (arr[i] == '--double-prev' && (i != 0)) {
+            newArr.push(newArr[newArr.length - 1]);
+            i++;
+        } else if ((arr[i] == '--double-prev') || (arr[i] == '--discard-prev') && (i === 0)) {
+            i++;
+        } else if ((arr[i] == '--discard-next') || (arr[i] == '--double-next') && (i === arr.length - 1)) {
+          return newArr;
+        } else {
+            newArr.push(arr[i]);
+            i++;
+        }
+  }
+  return newArr;
 };
