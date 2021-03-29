bab7f99e2366daab3220477e8ff6ccf7b93f208e
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-29 22:07:40 +0300

    fix: 1 fix

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,7 +3,8 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const Step = 2 ** disksNumber - 1;
   const fullTime = Math.floor(3600 / turnsSpeed * Step);
   
-     return = {
-       turns: Step, seconds: fullTime
-     };
+ return {
+    turns: Step,
+    seconds: fullTime
+     }
 };
