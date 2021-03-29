ca87ae7456b14201bf09acd70be7fa0d49e69c99
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-29 21:58:48 +0300

    fix: 2 fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,31 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-  module.exports = function repeater(str, options) {
-  
+module.exports = function repeater(str, options) {
   if (options.repeatTimes == undefined && options.additionRepeatTimes == undefined) {
     return str + options.addition;
   }
-       if (options.additionSeparator == undefined) {
-        options.additionSeparator = '|';
+  if (options.additionSeparator == undefined) {
+    options.additionSeparator = '|';
   }
-        if (options.separator == undefined) {
-          options.separator = '+';
+  if (options.separator == undefined) {
+    options.separator = '+';
   }
-        if (options.addition === null) {
-          options.addition = 'null';
-        }
+  if (options.addition === null) {
+    options.addition = 'null';
+  }
 
-  let array = [];
+  let arr = [];
+
   for (let i = 0; i < options.additionRepeatTimes; i++) {
-    array.push(options.addition);
+    arr.push(options.addition);
   }
 
-    
-  let str = array.join(options.additionSeparator);
-  let strNext = str + str;
-        let arrayStr = [];
-        for (let i = 0; i < options.repeatTimes; i++) {
-          arrayStr.push(strNext);
-        }
+  let str1 = arr.join(options.additionSeparator);
+  let str2 = str1 + str1;
+  let arrayStr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    arrayStr.push(str2);
+  }
    return arrayStr.join(options.separator);
 };
