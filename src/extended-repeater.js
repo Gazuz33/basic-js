f9da8c7ff3df99942340894cc165061710efa5e1
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-29 21:52:29 +0300

    feat: 1-st attemp

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  module.exports = function repeater(str, options) {
   
+  if (options.repeatTimes == undefined && options.additionRepeatTimes == undefined) {
+    return str + options.addition;
+  }
+       if (options.additionSeparator == undefined) {
+        options.additionSeparator = '|';
+  }
+        if (options.separator == undefined) {
+          options.separator = '+';
+  }
+        if (options.addition === null) {
+          options.addition = 'null';
+        }
+
+  let array = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    array.push(options.addition);
+  }
+  let arrayStr = [];
+    
+  let str = array.join(options.additionSeparator);
+  let strNext = str + str;
+        for (let i = 0; i < options.repeatTimes; i++) {
+          arrayStr.push(strNext);
+        }
+   return arrayStr.join(options.separator);
+};
